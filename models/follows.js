e52954bd62bfce297d53c5db7077f23746901a10
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-10-31 10:24:27 -0500

    added endpoints for follows

--- models/follows.js
+++ models/follows.js
@@ -1,28 +1,27 @@
-import { Schema } from "mongoose";
+import { Schema, model } from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
-
-
 const FollowSchema = Schema({
-    following_user: {
-        type: Schema.ObjectId,
-        ref: "User",
-        required: true
-    },
-    followed_user: {
-        type: Schema.ObjectId,
-        ref: "User",
-        required: true
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    }
-
+  following_user: {
+    type: Schema.ObjectId,
+    ref: "User",
+    required: true
+  },
+  followed_user: {
+    type: Schema.ObjectId,
+    ref: "User",
+    required: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
+// Definir un índice único para evitar seguir varias veces al mismo usuario
 FollowSchema.index({ following_user: 1, followed_user: 1 }, { unique: true});
-FollowSchema.plugin(mongoosePaginate)
+
+// Configrar el plugin de paginación
+FollowSchema.plugin(mongoosePaginate);
 
 export default model("Follow", FollowSchema, "follows");
-
